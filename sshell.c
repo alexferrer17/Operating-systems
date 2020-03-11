9b2185abf5d66f2e7d0bbff55ba211b6a47b7aa4
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2020-03-10 19:29:21 -0700

    Create sshell.c

---
+++ sshell.c
@@ -0,0 +1,9 @@
+/* 
+------------------------------------------------------------------------------------------
+
+this is the a sshell programme where a user inputs a command that will be read by a parent procces and then 
+transmited to a child procces where it will be executed we will do this by creating a uniderictional pipe.
+------------------------------------------------------------------------------------------
+*/
+
+
